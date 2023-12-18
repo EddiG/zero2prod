458c37bbc7c719267dc92c6f86d28696729e905f
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2023-12-19 00:49:29 +0500

    Adjust happy path assertions

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,6 @@
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configutation;
+
 async fn spawn_app() -> String {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:0")
         .await
@@ -30,6 +33,11 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let address = spawn_app().await;
     let client = reqwest::Client::new();
+    let configuration = get_configutation().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
 
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -43,6 +51,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(reqwest::StatusCode::OK, response.status());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Faild to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
