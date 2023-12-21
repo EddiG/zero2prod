7d0ed7ddd0d6d5935f7d45bdcd7543cb9ca0fa9e
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2023-12-21 18:17:01 +0500

    Repair tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -127,7 +127,7 @@ async fn subscribe_returns_a_422_when_data_is_missing() {
 
         // Assert
         assert_eq!(
-            reqwest::StatusCode::INTERNAL_SERVER_ERROR,
+            reqwest::StatusCode::UNPROCESSABLE_ENTITY,
             response.status(),
             "The API did not fail with 422 Unprocessable Entity when the payload was {}.",
             error_message
